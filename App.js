dedf6e00785024e601045a97d3ecab576bd11bb6
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-06-04 13:08:43 +0700

    add fitur tabs (in app.js)

--- App.js
+++ App.js
@@ -1,14 +1,57 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./src/screens/HomeScreen";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import ProfileScreen from "./src/screens/ProfileScreen";
+import CartScreen from "./src/screens/CartScreen";
+import MenuScreen from "./src/screens/MenuScreen";
 
 export default function App() {
 	const Tab = createBottomTabNavigator();
 
 	return (
 		<NavigationContainer>
-			<Tab.Navigator>
-				<Tab.Screen name="home" component={HomeScreen} />
+			<Tab.Navigator
+				screenOptions={{
+					tabBarShowLabel: false,
+					tabBarActiveTintColor: "#02c3d9",
+				}}>
+				<Tab.Screen
+					name="home"
+					component={HomeScreen}
+					options={{
+						tabBarIcon: ({ color, size }) => (
+							<Icon name="home" size={size} color={color} />
+						),
+					}}
+				/>
+				<Tab.Screen
+					name="account"
+					component={ProfileScreen}
+					options={{
+						tabBarIcon: ({ color, size }) => (
+							<Icon name="account" size={size} color={color} />
+						),
+					}}
+				/>
+				<Tab.Screen
+					name="cart"
+					component={CartScreen}
+					options={{
+						tabBarIcon: ({ color, size }) => (
+							<Icon name="cart" size={size} color={color} />
+						),
+					}}
+				/>
+				<Tab.Screen
+					name="menu"
+					component={MenuScreen}
+					options={{
+						tabBarIcon: ({ color, size }) => (
+							<Icon name="menu" size={size} color={color} />
+						),
+					}}
+				/>
 			</Tab.Navigator>
 		</NavigationContainer>
 	);
